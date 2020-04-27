64ffd97fc6f30640764fc9b980ca88de49859871
pfistfl <pfistererf@googlemail.com>
2020-04-27 11:06:01 +0200

    Update tic

--- tic.R
+++ tic.R
@@ -4,7 +4,7 @@ do_package_checks(args = "--as-cran")
 
 get_stage("install") %>% 
   add_code_step(keras::install_keras(tensorflow = "2.1.0", extra_packages = c("IPython", "requests", "certifi", "urllib3"))) %>%
-  add_code_step(tensorflow::tf_config())
+  add_code_step(tensorflow::tf_config()) %>%
   add_code_step(keras::install_keras(extra_packages = c("tensorflow-hub", "tabnet==0.1.4.1")))
 
 if (ci_on_ghactions()) {
