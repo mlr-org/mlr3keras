5372c2cdb13609f779f5776a706150674a46c66b
pfistfl <pfistererf@googlemail.com>
2020-04-28 10:16:04 +0200

    tic

--- tic.R
+++ tic.R
@@ -3,9 +3,9 @@ do_package_checks(args = "--as-cran")
 
 
 get_stage("install") %>%
-  add_code_step(keras::install_keras(tensorflow = "2.1.0", extra_packages = c("IPython", "requests", "certifi", "urllib3"))) %>%
-  add_code_step(tensorflow::tf_config()) %>%
-  add_code_step(keras::install_keras(extra_packages = c("tensorflow-hub", "tabnet==0.1.4.1")))
+  add_code_step(keras::install_keras(tensorflow = "2.1.0",
+    extra_packages = c("IPython", "requests", "certifi", "urllib3", "tensorflow-hub", "tabnet==0.1.4.1"))) %>%
+  add_code_step(tensorflow::tf_config())
 
 if (ci_on_ghactions()) {
   # creates pkgdown site and pushes to gh-pages branch
