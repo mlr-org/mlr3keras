5757d0d665f04c138cba1644796ba27dea9d63c8
Florian <pfistfl@users.noreply.github.com>
2020-06-29 17:17:57 +0200

    Update tic.R

--- tic.R
+++ tic.R
@@ -1,5 +1,5 @@
 # installs dependencies, runs R CMD check, runs covr::codecov()
-do_package_checks(args = c("--as-cran", "--no-manual", "--no-build-vignettes")
+do_package_checks(args = c("--as-cran", "--no-manual", "--no-build-vignettes"))
 
 if (ci_on_ghactions()) {
   # creates pkgdown site and pushes to gh-pages branch
