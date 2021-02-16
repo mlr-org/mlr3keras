80ed20c385f25c7ce5c59b0419f3be90bc8d7675
pfistfl <pfistererf@googlemail.com>
2021-02-16 11:01:29 +0100

    export

--- R/images_helpers.R
+++ R/images_helpers.R
@@ -4,6 +4,7 @@
 #'   List of dirs to create dataframes from.
 #' @return [`data.frame`]\cr
 #'   with columns "image" (an imagepath) and "class" (the class).
+#' @export
 imagepathdf_from_imagenet_dir = function(dirs) {
   rbindlist(map(dirs, function(y) {
     dt = rbindlist(map(list.dirs(y, recursive = FALSE), function(x) {
